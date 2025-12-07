0bc60329c87e59a1431e254684c3a78a75d8ae73
prince nyarko <princenyarko360@gmail.com>
2025-12-07 22:26:52 +0000

    Modify serializers.py for accounts serializer

--- school_management_system/accounts/serializers.py
+++ school_management_system/accounts/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from .models import CustomUser
 
+# Serializer for the CustomUser model
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'role']
-        read_only_fields = ['id', 'role']
+        fields = ['id', 
+                  'username', 'first_name', 
+                  'last_name', 
+                  'email', 
+                  'role'] # Fields to be included in the serialization
+        
+        read_only_fields = ['id', 'role'] # id and role are read-only
