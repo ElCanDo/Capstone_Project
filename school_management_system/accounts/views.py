b4142f4be09df7b3737ae26b88a1e26d14f31570
prince nyarko <princenyarko360@gmail.com>
2025-12-07 22:30:31 +0000

    Modify views.py to include viewsets for custom user

--- school_management_system/accounts/views.py
+++ school_management_system/accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from . models import CustomUser
+from . serializers import CustomUserSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.object.all()
+    serializer_class = CustomUserSerializer()
+    
