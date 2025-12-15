38146c41dd9ea9348a060114f8894f39092e9f82
prince nyarko <princenyarko360@gmail.com>
2025-12-15 20:06:25 +0000

    Add serializer.py to serialize all models in the project

---
+++ school_management_system/school_api/serializers.py
@@ -0,0 +1,49 @@
+from rest_framework import serializers
+from .models import CustomUser, Classroom, Teacher, Student, Enrollment, TeacherAssignment
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Mate:
+        model = CustomUser
+        fields = ['id', 
+                  'username', 'first_name', 
+                  'last_name', 
+                  'email', 
+                  'role'] 
+        
+        read_only_fields = ['id', 'role']
+
+class ClassroomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Classroom
+        fields = ['id', 'name']
+
+
+
+class TeacherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Teacher
+        fields = '__all__'
+        read_only_field = ['id', 'date_employed']
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'   
+
+        read_only_fields = ['id', 'enrollment_date']
+
+class EnrollmentSerializer(serializers.ModelSerializer):
+    student_name = serializers.CharField(source="student.full_name", read_only=True)
+    class_name = serializers.CharField(source="classroom.name", read_only=True)
+    class Meta:
+        model = Enrollment
+        fields =['id', 'student_full_name', 'classroom_name', 'date_enrolled' ]
+        read_only_field = ['id']
+
+class TeacherAssignSerializer(serializers.ModelSerializer):
+    teacher_name = serializers.CharField(source="teacher.full_name", read_only=True)
+    class_name = serializers.CharField(source="classroom.name", read_only=True)
+    class Meta:
+        model = TeacherAssignment
+        fields =['id', 'teacher_full_name', 'classroom_name']
+        read_only_field= ['id']
